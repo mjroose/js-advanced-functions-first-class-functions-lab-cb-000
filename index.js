85a1ccd7c5ee57b4072d2b160f333b196c7105cc
Michael Roose <mjroose@gmail.com>
2018-12-29 13:20:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const returnLastTwoDrivers = (drivers) => {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = (multiplier_outer) => {
-  return (value, multiplier_inner = multiplier_outer ) => {
-    return value * multiplier_inner;
+const createFareMultiplier = (multiplier) => {
+  return (value) => {
+    return value * multiplier;
   };
 };
